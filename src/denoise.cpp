83b51d6a9aea4668590b841e85bbf6947aa4cd9f
nohseonghun <jzi040941@naver.com>
2020-12-11 21:01:36 +0900

    filter strength calc function

--- src/denoise.cpp
+++ src/denoise.cpp
@@ -336,11 +336,12 @@ static int compute_frame_features(DenoiseState *st, kiss_fft_cpx *X, kiss_fft_cp
 }
 
 void pitch_filter(kiss_fft_cpx *X, const kiss_fft_cpx *P, const float *Ex, const float *Ep,
-                  const float *Exp, const float *g) {
+                  const float *Exp, const float *g, const float *r) {
   int i;
-  float r[NB_BANDS];
+  //float r[NB_BANDS];
   float rf[FREQ_SIZE] = {0};
   for (i=0;i<NB_BANDS;i++) {
+    /*
 #if 0
     if (Exp[i]>g[i]) r[i] = 1;
     else r[i] = Exp[i]*(1-g[i])/(.001 + g[i]*(1-Exp[i]));
@@ -352,6 +353,7 @@ void pitch_filter(kiss_fft_cpx *X, const kiss_fft_cpx *P, const float *Ex, const
 #endif
     r[i] *= sqrt(Ex[i]/(1e-8+Ep[i]));
   }
+  */
   interp_band_gain(rf, r);
   for (i=0;i<FREQ_SIZE;i++) {
     X[i].r = rf[i]*P[i].r;
@@ -395,3 +397,138 @@ float rnnoise_process_frame(DenoiseState *st, float *out, const float *in) {
   return 0;
 };
 
+void estimate_phat_corr(CommonState *st, float *Eyp, float *Ephatp){
+  for(int i=0; i<NB_BANDS; i++){
+    Ephatp[i] = Eyp[i]/sqrt((1-st->power_noise_attenuation)*Eyp[i] + st->power_noise_attenuation);
+  }
+}
+
+void filter_strength_calc(float *Exp, float *Eyp, float *Ephatp, float* r){
+  float alpha;
+  float a;
+  float b;
+  for(int i=0; i<NB_BANDS; i++){
+    a = Ephatp[i]*Ephatp[i] - Exp[i]*Exp[i];
+    b = Ephatp[i]*Eyp[i]*(1-Exp[i]*Exp[i]);
+    alpha = (sqrt(b*b + a *(Exp[i]*Exp[i]-Eyp[i]*Eyp[i]))-b)/a;
+    r[i] = alpha/(1+alpha);
+  }
+}
+
+
+static float uni_rand() {
+  return rand()/(double)RAND_MAX-.5;
+}
+
+static void rand_resp(float *a, float *b) {
+  a[0] = .75*uni_rand();
+  a[1] = .75*uni_rand();
+  b[0] = .75*uni_rand();
+  b[1] = .75*uni_rand();
+}
+
+int lowpass = FREQ_SIZE;
+int band_lp = NB_BANDS;
+
+int main(int argc, char **argv) {
+  int i;
+  int count=0;
+  static const float a_hp[2] = {-1.99599, 0.99600};
+  static const float b_hp[2] = {-2, 1};
+  float a_noise[2] = {0};
+  float b_noise[2] = {0};
+  float a_sig[2] = {0};
+  float b_sig[2] = {0};
+  float mem_hp_x[2]={0};
+  float mem_hp_n[2]={0};
+  float mem_resp_x[2]={0};
+  float mem_resp_n[2]={0};
+  float x[FRAME_SIZE];
+  float n[FRAME_SIZE];
+  float xn[FRAME_SIZE];
+  int vad_cnt=0;
+  int gain_change_count=0;
+  float speech_gain = 1, noise_gain = 1;
+  FILE *f1, *f2;
+  int maxCount;
+  DenoiseState *st;
+  DenoiseState *noise_state;
+  DenoiseState *noisy;
+  st = rnnoise_create(NULL);
+  noise_state = rnnoise_create(NULL);
+  noisy = rnnoise_create(NULL);
+  if (argc!=4) {
+    fprintf(stderr, "usage: %s <speech> <noise> <count>\n", argv[0]);
+    return 1;
+  }
+  f1 = fopen(argv[1], "r");
+  f2 = fopen(argv[2], "r");
+  maxCount = atoi(argv[3]);
+  for(i=0;i<150;i++) {
+    short tmp[FRAME_SIZE];
+    fread(tmp, sizeof(short), FRAME_SIZE, f2);
+  }
+  while (1) {
+    kiss_fft_cpx X[FREQ_SIZE], Y[FREQ_SIZE], N[FREQ_SIZE], P[WINDOW_SIZE];
+    float Ex[NB_BANDS], Ey[NB_BANDS], En[NB_BANDS], Ep[NB_BANDS];
+    float Exp[NB_BANDS];
+    float Ln[NB_BANDS];
+    float features[NB_FEATURES];
+    float g[NB_BANDS];
+    short tmp[FRAME_SIZE];
+    float vad=0;
+    float E=0;
+    if (count==maxCount) break;
+    if ((count%1000)==0) fprintf(stderr, "%d\r", count);
+    if (++gain_change_count > 2821) {
+      speech_gain = pow(10., (-40+(rand()%60))/20.);
+      noise_gain = pow(10., (-30+(rand()%50))/20.);
+      if (rand()%10==0) noise_gain = 0;
+      noise_gain *= speech_gain;
+      if (rand()%10==0) speech_gain = 0;
+      gain_change_count = 0;
+      rand_resp(a_noise, b_noise);
+      rand_resp(a_sig, b_sig);
+      lowpass = FREQ_SIZE * 3000./24000. * pow(50., rand()/(double)RAND_MAX);
+      for (i=0;i<NB_BANDS;i++) {
+        if (eband5ms[i]<<FRAME_SIZE_SHIFT > lowpass) {
+          band_lp = i;
+          break;
+        }
+      }
+    }
+    if (speech_gain != 0) {
+      fread(tmp, sizeof(short), FRAME_SIZE, f1);
+      if (feof(f1)) {
+        rewind(f1);
+        fread(tmp, sizeof(short), FRAME_SIZE, f1);
+      }
+      for (i=0;i<FRAME_SIZE;i++) x[i] = speech_gain*tmp[i];
+      for (i=0;i<FRAME_SIZE;i++) E += tmp[i]*(float)tmp[i];
+    } else {
+      for (i=0;i<FRAME_SIZE;i++) x[i] = 0;
+      E = 0;
+    }
+    if (noise_gain!=0) {
+      fread(tmp, sizeof(short), FRAME_SIZE, f2);
+      if (feof(f2)) {
+        rewind(f2);
+        fread(tmp, sizeof(short), FRAME_SIZE, f2);
+      }
+      for (i=0;i<FRAME_SIZE;i++) n[i] = noise_gain*tmp[i];
+    } else {
+      for (i=0;i<FRAME_SIZE;i++) n[i] = 0;
+    }
+    biquad(x, mem_hp_x, x, b_hp, a_hp, FRAME_SIZE);
+    biquad(x, mem_resp_x, x, b_sig, a_sig, FRAME_SIZE);
+    biquad(n, mem_hp_n, n, b_hp, a_hp, FRAME_SIZE);
+    biquad(n, mem_resp_n, n, b_noise, a_noise, FRAME_SIZE);
+    for (i=0;i<FRAME_SIZE;i++) xn[i] = x[i] + n[i];
+    frame_analysis(st, Y, Ey, x);
+    frame_analysis(noise_state, N, En, n);
+    for (i=0;i<NB_BANDS;i++) Ln[i] = log10(1e-2+En[i]);
+    int silence = compute_frame_features(noisy, X, P, Ex, Ep, Exp, features, xn);
+  }
+  fclose(f1);
+  fclose(f2);
+}
