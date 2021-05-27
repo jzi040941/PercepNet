f8ef5ce8c133b9a76f0b6e96f183a9772fecea19
Seonghun Noh <jzi040941@naver.com>
2021-05-27 16:12:05 +0900

    rnn_train first version

--- rnn_train.py
+++ rnn_train.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, Dataset
 import numpy as np
+import h5py
 
 class h5Dataset(Dataset):
 
@@ -18,16 +19,18 @@ class h5Dataset(Dataset):
         self.nb_sequences = len(all_data)//window_size
         print(self.nb_sequences, ' sequences')
         x_train = all_data[:self.nb_sequences*self.window_size, :self.x_dim]
-        x_train = np.reshape(x_train, (self.nb_sequences, self.window_size, self.x_dim))
+        self.x_train = np.reshape(x_train, (self.nb_sequences, self.window_size, self.x_dim))
+        #pad 3 for each batch .. not sure it's right
+        self.x_train = np.pad(self.x_train,[(0,0),(3,3),(0,0)],'constant')
 
-        y_train = np.copy(all_data[:self.nb_sequences*self.window_size, self.y_dim])
-        y_train = np.reshape(y_train, (self.nb_sequences, self.window_size, self.y_dim))
+        y_train = np.copy(all_data[:self.nb_sequences*self.window_size, self.x_dim:self.x_dim+self.y_dim])
+        self.y_train = np.reshape(y_train, (self.nb_sequences, self.window_size, self.y_dim))
 
     def __len__(self):
         return self.nb_sequences
 
     def __getitem__(self, index):
-        return (x_train[index], y_train[index])
+        return (self.x_train[index], self.y_train[index])
 
 class PercepNet(nn.Module):
     def __init__(self, input_dim=70):
@@ -53,11 +56,11 @@ class PercepNet(nn.Module):
         convout = convout.permute([0,2,1]) # B, T, D
         gruout, grustate = self.gru(convout)
         
-        rnn_gb_out = self.gru_gb(gruout,grustate[3:])
-        gb = self.fc_gb(self.rnn_gb_out)
+        rnn_gb_out = self.gru_gb(gruout)
+        gb = self.fc_gb(rnn_gb_out[0])
 
-        rnn_rb_out = self.gru_rb(gruout,grustate[3:])
-        rb = self.fc_rb(self.rnn_rb_out)
+        rnn_rb_out = self.gru_rb(gruout)
+        rb = self.fc_rb(rnn_rb_out[0])
         return gb,rb
 
 def test():
@@ -74,50 +77,50 @@ class CustomLoss(nn.Module):
         gamma = 0.5
         C4 = 10
 
-        rb_hat = output[:34]
-        gb_hat = output[34:68]
-        rb = target[:34]
-        gb = target[34:68]
+        rb_hat = outputs[0]
+        gb_hat = outputs[1]
+        rb = targets[:,:,:34]
+        gb = targets[:,:,34:68]
 
-        return ((gb**gamma - gb_hat**gamma)**2).sum() + C4*((gb**gamma - gb_hat**gamma)**4).sum() +
-        (((1-rb)**gamma-(1-rb_hat)**gamma)**2).sum()
+        return ((gb**gamma - gb_hat**gamma)**2).sum() + C4*((gb**gamma - gb_hat**gamma)**4).sum() + (((1-rb)**gamma-(1-rb_hat)**gamma)**2).sum()
 
 def train():
     dataset = h5Dataset("training.h5")
     trainset_ratio = 0.8 # 1 - validation set ration
     train_size = int(trainset_ratio * len(dataset))
     test_size = len(dataset) - train_size
+    batch_size=2
     train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
     
     train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
     validation_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size)
 
     model = PercepNet()
-    optimizer = optim.Adam(model.parameter(), lr=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
     criterion = CustomLoss()
     num_epochs = 2
     for epoch in range(num_epochs):  # loop over the dataset multiple times
 
-    running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, targets = data
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, targets = data
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+            # zero the parameter gradients
+            optimizer.zero_grad()
 
-        # forward + backward + optimize
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
-        loss.backward()
-        optimizer.step()
+            # forward + backward + optimize
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            loss.backward()
+            optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
+            # print statistics
+            running_loss += loss.item()
+            if i % 2000 == 1999:    # print every 2000 mini-batches
+                print('[%d, %5d] loss: %.3f' %
+                    (epoch + 1, i + 1, running_loss / 2000))
+                running_loss = 0.0
 
     print('Finished Training')
-test()
+train()
